932129b9e2ca7665f49f0d236e9c18abd5a22893
doDomoGu <kaka_71936410@qq.com>
2017-08-07 21:12:52 +0800

    通用model

--- models/users.js
+++ models/users.js
@@ -1,6 +1,8 @@
-var redisClient = require('../components/redis');
+var models = require('./_models');
 
-var user = {
+models.modelName = 'users';
+
+models.model = {
     name: "",
     password: "",
     sex: 0,
@@ -9,119 +11,6 @@ var user = {
     status: 1
 };
 
-module.exports.list = function(callback){
-    redisClient.hgetall('users', function(error, res){
-        if(error) {
-            console.log(error);
-        } else {
-            var result = {};
-            if(res!=null){
-                for(var i in res){
-                    var resTemp = JSON.parse(res[i]);
-                    var resOne = {};
-                    for(var j in user){
-                        resOne[j] = resTemp[j]!=undefined?resTemp[j]:user[j];
-                    }
-                    result[i] = resOne;
-                }
-            }
-            callback(null,result);
-        }
-    });
-};
-
-module.exports.get = function(id,callback){
-    redisClient.hgetall('users', function(error, res){
-        if(error) {
-            console.log(error);
-        } else {
-            var result = {};
-            if(res[id]){
-                var resTemp = JSON.parse(res[id]);
-                for(var i in user){
-                    result[i] = resTemp[i]!=undefined?resTemp[i]:user[i];
-                }
-            }
-            callback(null,result);
-        }
-    });
-};
-
-module.exports.add = function(data,callback){
-    redisClient.hlen('users',function(error,res){
-        if(error) {
-            console.log(error);
-        } else {
-            //res = 数据长度
-            var sub_key = res + 1;
-            //为data增加一个值 id
-            //data.id = sub_key;
-
-            var dataOne = {};
-            for(var i in user){
-                dataOne[i] = data[i]!=undefined?data[i]:user[i];
-            }
-
-            redisClient.hset('users', sub_key , JSON.stringify(dataOne) , function(error, res) {
-                if (error) {
-                    console.log(error);
-                } else {
-                    callback(null,{id:sub_key});
-                }
-            })
-        }
-    });
-};
 
 
-module.exports.delete = function(id,callback){
-    redisClient.hgetall('users', function(error, res){
-        if(error) {
-            console.log(error);
-        } else {
-            if(res!=undefined && res[id]!=undefined){
-                var data = JSON.parse(res[id]);
-                data.status = 0;
-                redisClient.hset('users', id , JSON.stringify(data) , function(error, res) {
-                    if (error) {
-                        console.log(error);
-                    } else {
-                        callback(null,{id:id,message:'delete ok!'});
-                    }
-                })
-            }else{
-                callback(null,{id:id,message:'id wrong'});
-            }
-        }
-    });
-};
-
-
-module.exports.update = function(id, dataNew, callback){
-    redisClient.hgetall('users', function(error, res){
-        if(error) {
-            console.log(error);
-        } else {
-            if(res!=undefined && res[id]!=undefined){
-                var dataOld = JSON.parse(res[id]);
-                var data = {};
-                for(var i in user){
-                    data[i] = dataOld[i]!=undefined?(dataNew[i]!=undefined?dataNew[i]:dataOld[i]):user[i];
-                }
-                redisClient.hset('users', id , JSON.stringify(data) , function(error, res) {
-                    if (error) {
-                        console.log(error);
-                    } else {
-                        callback(null,{id:id,message:'update ok!'});
-                    }
-                })
-            }else{
-                callback(null,{id:id,message:'id wrong'});
-            }
-        }
-    });
-};
-
-
-
-
+module.exports = models;
