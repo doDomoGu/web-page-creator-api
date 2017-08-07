ef8d817bd8ebfdcb652c4e50047604304beae090
doDomoGu <kaka_71936410@qq.com>
2017-08-07 20:40:22 +0800

    users 更新操作

--- routes/users.js
+++ routes/users.js
@@ -24,7 +24,16 @@ router.post('/', function(req, res) {
     });
 });
 
-
+//更新(修改)
+router.patch('/:id', function(req, res) {
+    users.update(req.params.id,req.body,function(err, data){
+        if (err) {
+            console.error(err);
+        } else {
+            res.json(data);
+        }
+    });
+});
 
 //获取单条
 router.get('/:id', function(req, res) {
