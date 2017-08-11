44d303706aaf1acd9d976c82fab76ac0b613fe51
doDomoGu <kaka_71936410@qq.com>
2017-08-11 11:50:18 +0800

    路由分离

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,67 @@
-var router = require('./_routers');
+var express = require('express');
+var router = express.Router();
+var users = require('../models/users');
 
-_router = new router(require('../models/users'));
 
-module.exports = _router;
+//获取全部
+router.get('/', function(req, res) {
+    users.list(function(err, data){
+        if (err) {
+            console.error(err);
+        } else {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+            res.header("Access-Control-Allow-Headers", "X-Requested-With");
+            res.header('Access-Control-Allow-Headers', 'Content-Type');
+            res.json(data);
+        }
+    });
+});
 
+//新增
+router.post('/', function(req, res) {
+    users.add(req.body,function(err, data){
+        if (err) {
+            console.error(err);
+        } else {
+            res.json(data);
+        }
+    });
+});
+
+//更新(修改)
+router.patch('/:id', function(req, res) {
+    users.update(req.params.id,req.body,function(err, data){
+        if (err) {
+            console.error(err);
+        } else {
+            res.json(data);
+        }
+    });
+});
+
+//获取单条
+router.get('/:id', function(req, res) {
+    users.get(req.params.id,function(err, data){
+        if (err) {
+            console.error(err);
+        } else {
+            res.json(data);
+        }
+    });
+});
+
+//删除单条 （逻辑删除）
+router.delete('/:id', function(req, res) {
+    users.delete(req.params.id,function(err, data){
+        if (err) {
+            console.error(err);
+        } else {
+            res.json(data);
+        }
+    });
+});
+
+
+module.exports = router;
 
--- routes/websites.js
+++ routes/websites.js
@@ -1,10 +1,67 @@
-var router = require('./_routers');
-
-_model = require('../models/websites')
-
-_router = new router(_model);
+var express = require('express');
+var router = express.Router();
+var websites = require('../models/websites');
 
 
-module.exports = _router;
+//获取全部
+router.get('/', function(req, res) {
+    websites.list(function(err, data){
+        if (err) {
+            console.error(err);
+        } else {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+            res.header("Access-Control-Allow-Headers", "X-Requested-With");
+            res.header('Access-Control-Allow-Headers', 'Content-Type');
+            res.json(data);
+        }
+    });
+});
 
+//新增
+router.post('/', function(req, res) {
+    websites.add(req.body,function(err, data){
+        if (err) {
+            console.error(err);
+        } else {
+            res.json(data);
+        }
+    });
+});
+
+//更新(修改)
+router.patch('/:id', function(req, res) {
+    websites.update(req.params.id,req.body,function(err, data){
+        if (err) {
+            console.error(err);
+        } else {
+            res.json(data);
+        }
+    });
+});
+
+//获取单条
+router.get('/:id', function(req, res) {
+    websites.get(req.params.id,function(err, data){
+        if (err) {
+            console.error(err);
+        } else {
+            res.json(data);
+        }
+    });
+});
+
+//删除单条 （逻辑删除）
+router.delete('/:id', function(req, res) {
+    websites.delete(req.params.id,function(err, data){
+        if (err) {
+            console.error(err);
+        } else {
+            res.json(data);
+        }
+    });
+});
+
+
+module.exports = router;
 
