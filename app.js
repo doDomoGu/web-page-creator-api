377545d022b59f4c9a85f6e11278a6930e880a2f
doDomoGu <kaka_71936410@qq.com>
2017-08-17 22:37:46 +0800

    用户 登录 auth认证

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var websites = require('./routes/websites');
 var users = require('./routes/users');
+var auths = require('./routes/auths');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/websites', websites);
+app.use('/auths', auths);
 
 
 // catch 404 and forward to error handler
