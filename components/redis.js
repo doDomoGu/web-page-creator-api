cd6287af8d7c00a5c77577a7e714a09e5ecdebd3
doDomoGu <kaka_71936410@qq.com>
2017-08-24 14:08:01 +0800

    部署服务器

--- components/redis.js
+++ components/redis.js
@@ -2,13 +2,19 @@ var redis = require('redis');
 var config = require('config');
 var options = {
     host: config.get('redis.host'),
-    port: config.get('redis.port')
+    port: config.get('redis.port'),
+    password: config.get('redis.password')
 };
 var client = redis.createClient(options);
 var dbIndex = config.get('redis.index');
 client.on('ready', function () {
     console.log('redis connected');
 });
+if(options.password){
+    client.auth(options.password,function(){
+        console.log('通过认证');
+    });
+}
 
 client.select(dbIndex,function(error){
     if(error) {
