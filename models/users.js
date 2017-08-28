8c405898714ab46a1f6041d1af553ae0cd002385
doDomoGu <kaka_71936410@qq.com>
2017-08-28 18:05:14 +0800

    登录验证 mysql

--- models/users.js
+++ models/users.js
@@ -1,5 +1,6 @@
-var redisClient = require('../components/redis');
+//var redisClient = require('../components/redis');
 var models = require('./_models');
+var mysql = require('../components/mysql');
 var jwt = require('jsonwebtoken');
 
 var users = new models(
@@ -28,39 +29,97 @@ var auth_roles = {
     2:['user_admin','website_admin'],
     3:['user_admin']
 };
+
+
 function generateToken(user_id){
     var token = jwt.sign(
-        {
-            user_id:user_id
-        },
-        Math.floor(Math.random()*(10000-10+1)+10).toString(),
-        {
-            expiresIn: 60*60*24  // 24小时过期
-        }
-    );
-    var tokenExist = false;
-    redisClient.hgetall('wpc_jwt', function(error, res){
-        if(error) {
-            console.log(error);
-        } else {
-            for(var i in res){
-                if(i==token){
-                    tokenExist = true;
-                }
+            {
+                user_id:user_id
+            },
+            Math.floor(Math.random()*(10000-10+1)+10).toString(),
+            {
+                expiresIn: 60*60*24  // 24小时过期
             }
-        }
+        );
+    //#######  忽略重复token的问题  ########
+
+    /*var tokenExist = false;
+
+    mysql.query('SELECT * FROM `user_auth_token` where token = ?',token, function (error, res) {
+        if (error) throw error;
+
     });
+
 
     if(tokenExist){
         token = generateToken(user_id);
     }
+
+    */
 
     return token;
 }
 
 
 users.auth = function(data,callback){
-    redisClient.hgetall(users.modelName, function(error, res){
+    var that = this;
+
+    //查用户名
+    mysql.query('SELECT * FROM `' + that.modelName + '` where username = ?',data.username, function (error, res) {
+        if (error){
+            return callback(error);
+        }
+        var result = {
+            error_msg : '未知错误',
+            success : false,
+            token : '',
+            user_id : 0,
+            roles : []
+        };
+        if(res.length == 1){
+            var _res = res[0];
+
+            if(res[0].password==data.password){
+                result.success = true;
+                result.error_msg = false;
+                result.user_id = _res.id;
+                result.roles = auth_roles[_res.id]!=undefined?auth_roles[_res.id]:[];
+                var token = generateToken(_res.id);
+                result.token = token;
+                var expired = new Date(new Date().getTime() + 60*60*24*1000);
+
+                var expired_time = '';
+                expired_time += expired.getFullYear();
+                expired_time += "-"+(expired.getMonth()+1 < 10 ? '0' + (expired.getMonth()+1) : (expired.getMonth()+1));
+                expired_time += "-"+(expired.getDate() < 10 ? '0' + expired.getDate() : expired.getDate());
+                expired_time += " "+expired.getHours();
+                expired_time += ":"+expired.getMinutes();
+                expired_time += ":"+expired.getSeconds();
+
+
+                mysql.query('INSERT INTO `user_auth_token` SET ?',{user_id:_res.id,token:token,expired_time:expired_time}, function (error, res) {
+                    if (error) throw error;
+
+                    //return callback(null,{id:res.insertId});
+                });
+
+
+
+
+
+            }else{
+                result.error_msg = '用户名或密码错误 (001)';
+                //return callback(null,{error:'don\'t find the user'});
+            }
+
+        }else{
+            //return callback(null,{error:'don\'t find the user'});
+            result.error_msg = '用户名或密码错误 (002)';
+        }
+        return callback(null,result);
+    });
+
+    /*redisClient.hgetall(users.modelName, function(error, res){
         if(error) {
             console.log(error);
         } else {
@@ -108,7 +167,7 @@ users.auth = function(data,callback){
             callback(null,result);
         }
         //callback(error,{});
-    });
+    });*/
 };
 
 users.getUserInfoByToken = function(data,callback){
